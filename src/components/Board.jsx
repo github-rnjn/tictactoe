8c372c6f91c328426767b6111733d0958ef281a4
Ranjan <ranjankumar4652@gmail.com>
2025-02-11 21:56:19 +0530

    add winner calculation and status display to Tic Tac Toe board

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -5,8 +5,9 @@ import { useState } from "react"
 const Board = () => {
     const [squares,setSquares] = useState(Array(9).fill(null));
     const [xIsNext,setXIsNext] = useState(true)
+
     const handleClick = (i)=>{
-        if(squares[i]){
+        if(squares[i] || calculateWinner(squares)){
             return;
         }
         const nextSquares = squares.slice();
@@ -18,8 +19,36 @@ const Board = () => {
         setSquares(nextSquares)
         setXIsNext(!xIsNext)
     }
+
+    const calculateWinner = (squares)=>{
+        const lines = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+        ]
+        for(let i=0;i<lines.length;i++){
+            const [a,b,c] = lines[i];
+            if(squares[a] && squares[a]===squares[b] && squares[a]===squares[c]){
+                return squares[a];
+            }
+        }
+        return null;
+    }
+    const winner = calculateWinner(squares)
+    let status;
+    if(winner){
+        status = "Winner is: "+winner;
+    }else{
+        status = "Next Player: "+(xIsNext?"X":"O");
+    }
     return (
         <>
+            <h1>{status}</h1>
             <div className="board-row">
                 <Square value={squares[0]} onSquareClick={()=>handleClick(0)} />
                 <Square value={squares[1]} onSquareClick={()=>handleClick(1)} />
