5d646c6248ecf4e925ad9f2270aeac549eaf3972
Ranjan <ranjankumar4652@gmail.com>
2025-02-11 21:20:25 +0530

    initialize React app with Vite setup and basic Tic Tac Toe components

---
+++ src/components/Board.jsx
@@ -0,0 +1,26 @@
+import Square from "./Square"
+import "../css/board.css"
+
+const Board = () => {
+    return (
+        <>
+            <div className="board-row">
+                <Square />
+                <Square />
+                <Square />
+            </div>
+            <div className="board-row">
+                <Square />
+                <Square />
+                <Square />
+            </div>
+            <div className="board-row">
+                <Square />
+                <Square />
+                <Square />
+            </div>
+        </>
+    )
+}
+
+export default Board
---
+++ src/components/Square.jsx
@@ -0,0 +1,9 @@
+import "../css/square.css"
+
+const Square = () => {
+    return (
+        <button className="square">X</button>
+    )
+}
+
+export default Square
